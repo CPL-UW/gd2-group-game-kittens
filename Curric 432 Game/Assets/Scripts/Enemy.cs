bdeda2a91434fbbd789b557eb67018e8290bef80
atat2 <71809445+atat2@users.noreply.github.com>
2022-11-09 21:26:34 -0600

    Changed Enemy Script Code
    Changed code so that enemies will only move towards player if within a range

--- Curric 432 Game/Assets/Scripts/Enemy.cs
+++ Curric 432 Game/Assets/Scripts/Enemy.cs
@@ -6,10 +6,15 @@ using System;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _speed = 0.125f;
+    [SerializeField] private float _speed;
     Rigidbody2D rb;
     Transform target;
     Vector2 moveDirection;
+
+    float distance; //distance between enemy and target
+    public float aggroRange; //Range between enemy and target for AI
+    public GameObject spawn; //Location enemy will go to if not targeting player
+    public Transform player; //Player
 
     // Start is called before the first frame update
     void Awake()
@@ -20,11 +25,15 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (target != null)
         {
             Vector3 direction = (target.position - transform.position).normalized;
@@ -32,10 +41,20 @@ public class Enemy : MonoBehaviour
             rb.rotation = angle;
             moveDirection = direction;
         }
+
     }
 
     void FixedUpdate()
     {
+
+        //Checks distance between enemy and target
+        distance = Vector2.Distance(player.position, transform.position);
+
+        if(distance <= aggroRange) {
+            target = player;
+        } else {
+            target = spawn.transform;
+        }
         rb.MovePosition((Vector2)transform.position + (moveDirection * _speed * Time.deltaTime));
     }
 }
