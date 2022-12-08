a9a29a43491cf6e13bc401372b8713e84f95914c
atat2 <71809445+atat2@users.noreply.github.com>
2022-12-07 19:32:37 -0600

    Added new tiles and End Screen function
    Added new tiles and collision
    End screen has images and text that says who won

--- Curric 432 Game/Assets/Scripts/EndMenu.cs
+++ Curric 432 Game/Assets/Scripts/EndMenu.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndMenu : MonoBehaviour
 {
+    public Text winText;
+    public Image Player;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(VariableController.PlayerWhoWon);
         if(VariableController.PlayerWhoWon == 1) {
+            winText.text = "Player 1 Wins";
+            Player.color = Color.white;
             Debug.Log("Player 1 Wins");
         }
         if(VariableController.PlayerWhoWon == 2) {
+            winText.text = "Player 2 Wins";
+            Player.color = Color.blue;
             Debug.Log("Player 2 Wins");
         }
     }
